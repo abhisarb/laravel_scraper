c4e64b1a2898d4554dfd89062ad2a114dd8ff309
Sarbottam Abhijeet <abhijeetsarbottam@gmail.com>
2025-12-24 14:54:42 +0530

    Add /init route for database setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/init', function () {
+    try {
+        Artisan::call('migrate --force');
+        $output = Artisan::output();
+        
+        Artisan::call('scrape:blogs');
+        $output .= "\n" . Artisan::output();
+        
+        return "<pre>Setup Complete!\n\n$output</pre>";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
 });
